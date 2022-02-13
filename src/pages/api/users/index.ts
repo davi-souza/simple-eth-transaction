c815302508c17ecc3a66b9ca9f9d88e8464c0b58
Davi Souza <ddas.souza@gmail.com>
2022-02-13 18:59:52 -0300

    updates start command

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -6,6 +6,7 @@ import {
   userToApiResponse,
 } from '../../../services/api';
 import { eth } from '../../../services/blockchain/commons';
+import { encryptString } from '../../../services/cryptography';
 import { validateCreateUserPayload } from '../../../services/validation';
 
 const usersApi = routeHandler({
@@ -21,11 +22,6 @@ const usersApi = routeHandler({
     );
   },
   get: privateRouteHandler(async (req, res) => {
-    const x = await eth.getPastLogs({
-      fromBlock: '0x0',
-      address: req.user.address,
-    });
-    console.log(x);
     return res
       .status(200)
       .json(toApiResponseFormat(userToApiResponse(req.user)));
